3cf3c96ba4a60410e73a7947d729fc40c2b9ec0c
NandeBija <nandebija@gmail.com>
2022-03-01 12:17:55 +0200

    commit

--- index.js
+++ index.js
@@ -23,133 +23,133 @@ app.use("/users", userrouter); //Telling the app to use the user.. url is localh
 app.use("/products", Productrouter);
 app.use("/cart", cartrouter);
 
-// // API routes
-// app.get("/", (req, res, next) => {
-//   res.send({
-//     message: "Welcome to the Generic Blog API",
-//     user_routes: {
-//       user_register: {
-//         method: "POST",
-//         route: "/users",
-//         request_body: {
-//           name: "String",
-//           email: "String",
-//           contact: "String",
-//           password: "String",
-//         },
-//         result: {
-//           jwt: "String token",
-//         },
-//       },
-//       user_login: {
-//         method: "PATCH",
-//         route: "/users",
-//         request_body: {
-//           email: "String",
-//           password: "String",
-//         },
-//         result: {
-//           jwt: "String token",
-//         },
-//       },
-//       all_users: {
-//         method: "GET",
-//         route: "/users",
-//         result: {
-//           users: "Array",
-//         },
-//       },
-//       single_user: {
-//         method: "GET",
-//         route: "/users/:id",
-//         result: {
-//           user: "Object",
-//         },
-//       },
-//       update_user: {
-//         method: "PUT",
-//         request_body: {
-//           name: "String",
-//           email: "String",
-//           contact: "String",
-//           password: "String",
-//           avatar: "String",
-//           img: "String *optional* (Must be hosted image. I can suggest to host on Post Image)",
-//         },
-//         route: "/users/:id",
-//         result: {
-//           user: "Object",
-//         },
-//       },
-//       delete_user: {
-//         method: "DELETE",
-//         route: "/users/:id",
-//         result: {
-//           message: "Object",
-//         },
-//       },
-//     },
-//     post_routes: {
-//       all_posts: {
-//         method: "GET",
-//         route: "/posts",
-//         headers: {
-//           authorization: "Bearer (JWT token)",
-//         },
-//         result: {
-//           posts: "Array",
-//         },
-//       },
-//       single_post: {
-//         method: "GET",
-//         route: "/posts/:id",
-//         headers: {
-//           authorization: "Bearer (JWT token)",
-//         },
-//         result: {
-//           post: "Object",
-//         },
-//       },
-//       create_post: {
-//         method: "POST",
-//         route: "/posts/",
-//         headers: {
-//           authorization: "Bearer (JWT token)",
-//         },
-//         request_body: {
-//           title: "String",
-//           body: "String",
-//           img: "String *optional* (Must be hosted image. I can suggest to host on Post Image)",
-//         },
-//         result: {
-//           post: "Object",
-//         },
-//       },
-//       update_post: {
-//         method: "PUT",
-//         route: "/posts/:id",
-//         headers: {
-//           authorization: "Bearer (JWT token)",
-//         },
-//         request_body: {
-//           title: "String *optional*",
-//           body: "String *optional*",
-//           img: "String *optional* (Must be hosted image. I can suggest to host on Post Image)",
-//         },
-//         result: {
-//           post: "Object",
-//         },
-//       },
-//       delete_post: {
-//         method: "DELETE",
-//         route: "/posts/:id",
-//         result: {
-//           message: "Object",
-//         },
-//       },
-//     },
-//   });
-// });
+// API routes
+app.get("/", (req, res, next) => {
+  res.send({
+    message: "Welcome to the Generic Blog API",
+    user_routes: {
+      user_register: {
+        method: "POST",
+        route: "/users",
+        request_body: {
+          name: "String",
+          email: "String",
+          contact: "String",
+          password: "String",
+        },
+        result: {
+          jwt: "String token",
+        },
+      },
+      user_login: {
+        method: "PATCH",
+        route: "/users",
+        request_body: {
+          email: "String",
+          password: "String",
+        },
+        result: {
+          jwt: "String token",
+        },
+      },
+      all_users: {
+        method: "GET",
+        route: "/users",
+        result: {
+          users: "Array",
+        },
+      },
+      single_user: {
+        method: "GET",
+        route: "/users/:id",
+        result: {
+          user: "Object",
+        },
+      },
+      update_user: {
+        method: "PUT",
+        request_body: {
+          name: "String",
+          email: "String",
+          contact: "String",
+          password: "String",
+          avatar: "String",
+          img: "String *optional* (Must be hosted image. I can suggest to host on Post Image)",
+        },
+        route: "/users/:id",
+        result: {
+          user: "Object",
+        },
+      },
+      delete_user: {
+        method: "DELETE",
+        route: "/users/:id",
+        result: {
+          message: "Object",
+        },
+      },
+    },
+    post_routes: {
+      all_posts: {
+        method: "GET",
+        route: "/posts",
+        headers: {
+          authorization: "Bearer (JWT token)",
+        },
+        result: {
+          posts: "Array",
+        },
+      },
+      single_post: {
+        method: "GET",
+        route: "/posts/:id",
+        headers: {
+          authorization: "Bearer (JWT token)",
+        },
+        result: {
+          post: "Object",
+        },
+      },
+      create_post: {
+        method: "POST",
+        route: "/posts/",
+        headers: {
+          authorization: "Bearer (JWT token)",
+        },
+        request_body: {
+          title: "String",
+          body: "String",
+          img: "String *optional* (Must be hosted image. I can suggest to host on Post Image)",
+        },
+        result: {
+          post: "Object",
+        },
+      },
+      update_post: {
+        method: "PUT",
+        route: "/posts/:id",
+        headers: {
+          authorization: "Bearer (JWT token)",
+        },
+        request_body: {
+          title: "String *optional*",
+          body: "String *optional*",
+          img: "String *optional* (Must be hosted image. I can suggest to host on Post Image)",
+        },
+        result: {
+          post: "Object",
+        },
+      },
+      delete_post: {
+        method: "DELETE",
+        route: "/posts/:id",
+        result: {
+          message: "Object",
+        },
+      },
+    },
+  });
+});
 
 app.listen(app.get("port"), (server) => {
   console.info(`Server listen on port ${app.get("port")}`);
